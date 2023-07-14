15711a2a6ba71c09a387b3c9139c856f8b13ad66
abhi89 <abhilol899@gmail.com>
2023-07-14 11:38:59 +0100

    p5 is workinggit add .git add .

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps')
 const browserSync = require('browser-sync').create();
 
 gulp.task('html', () => {
@@ -7,21 +6,15 @@ gulp.task('html', () => {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass', () => {
-    return gulp.src('src/scss/**/*.sass')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(
-            cleanCSS({
-                compatibility: 'ie8',
-            }),
-        )
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/css'))
-        .pipe(browserSync.stream());
-});
+gulp.task('js', () => {
+    return gulp.src('src/js/*.js')
+        .pipe(gulp.dest('dist/js'));
+})
 
-
+gulp.task('libs', () => {
+    return gulp.src('src/libs/*.js')
+        .pipe(gulp.dest('dist/libs'));
+})
 
 gulp.task('watch', () => {
     browserSync.init({
@@ -31,12 +24,8 @@ gulp.task('watch', () => {
     });
 
     gulp.watch('src/*.html', gulp.series('html')).on('change', browserSync.reload);
-    gulp.watch('src/scss/**/*.sass', gulp.series('sass'));
-    gulp.watch('src/img/*.jpg', gulp.series('img'));
-    gulp.watch('src/js/*.js', gulp.series('js'));
-    gulp.watch('src/data/*.json', gulp.series('data')).on('change', browserSync.reload);
-
+    gulp.watch('src/js/*.js', gulp.series('js')).on('change', browserSync.reload);
 });
 
 
-gulp.task('default', gulp.series('html', 'fonts', 'sass', 'img', 'data', 'js', 'watch'));
+gulp.task('default', gulp.series('html', 'js','libs', 'watch'));
---
+++ dist/js/app.js
@@ -0,0 +1,10 @@
+
+function setup() {
+    createCanvas(600,600)
+    // background(0)
+}
+
+function draw() {
+    fill('#ff0000')
+    ellipse(300,300,50)
+}
