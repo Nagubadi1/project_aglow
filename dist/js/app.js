922e2c894d2092aad5ada38caf741bc2a1e548a4
abhi89 <abhilol899@gmail.com>
2023-08-01 16:38:19 +0100

    modified:   dist/js/app.js 	modified:   src/js/app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -34,7 +34,7 @@
  * 
  * Some other thoughts might be can we add x number of diamond shapes offset by an angle
  *    this would be nice to be progromatically generated i.e two diamonds are 90• offset, three would be 60• etc
- * 
+ *  
  * 
  * if you get the Light class working with an animated effect can we...
  * change the effect by passing an argument? 
@@ -49,31 +49,74 @@
  */
 
 
+
 class Light {
-  constructor(/* instance args here */ radius) {
-    this.radius = radius
+  constructor(x, y, radius) {
+    this.position = createVector(x, y);
+    this.radius = radius;
+    this.state = 255; // Set initial brightness to maximum
+    this.shiningSpeed = 0.1;
+    this.edgeOpacity = 50;
+    this.angleOffset = random(TWO_PI); // Random initial angle offset for rotation
+    this.rotationSpeed = 0.005; // Rotation speed of the diamond
+    this.pulseSpeed = 0.05; // Speed of the diamond pulsing animation
+    this.baseRadius = radius;
+    this.lensFlareOpacity = 50; // Opacity of the lens flare
+    this.lensFlareSize = 5; // Initial size of the lens flare
+    this.lensFlareSpacing = 5; // Spacing between lens flare ellipses
+    this.lensFlareCount = 5; // Number of lens flare ellipses
   }
 
-  // Methods - feel free to change these where appropriate
-
-  getPosition() {
-
+  update() {
+    // Update shining effect with a random factor to create a twinkling effect
+    this.state = (this.state + random(-10, 10)) % 255;
   }
 
-  offsetAngle() {
-
-  }
-  
   drawLight() {
+    // Draw shining effect (diamond shape) with lowered opacity on edges
+    const angleStep = TWO_PI / 4; // Angle step for diamond shape (4 points)
+    const brightness = map(this.state, 0, 255, 150, 255); // Increased brightness
+    const edgeOpacity = this.edgeOpacity; // Opacity of the diamond edges
+    fill(255, 255, 255, brightness); // White glare
+    noStroke();
+    beginShape();
+    for (let j = 0; j < 4; j++) {
+      let angle = (j * angleStep) + this.angleOffset;
+      let xOff = cos(angle) * this.radius;
+      let yOff = sin(angle) * this.radius;
+      let alpha = j % 2 === 0 ? 255 : edgeOpacity; // Lower opacity on edges
+      fill(255, 255, 255, alpha);
+      vertex(this.position.x + xOff, this.position.y + yOff);
+    }
+    endShape(CLOSE);
 
+    // Draw base light
+    fill(255);
+    ellipse(this.position.x, this.position.y, 6, 6);
+
+    // Draw lens flare effect
+    noStroke();
+    for (let i = 0; i < this.lensFlareCount; i++) {
+      let flareAlpha = map(i, 0, this.lensFlareCount - 1, this.lensFlareOpacity, 0);
+      fill(255, 255, 255, flareAlpha);
+      ellipse(this.position.x, this.position.y, this.lensFlareSize + i * this.lensFlareSpacing);
+    }
+  }
+
+  rotate() {
+    // Rotate the diamond around its center
+    this.angleOffset += this.rotationSpeed;
+  }
+
+  pulse() {
+    // Loop the diamond growing and shrinking
+    this.radius = this.baseRadius + sin(frameCount * this.pulseSpeed) * 5;
   }
 }
 
-
-const numLights = 10; // Set the number of lights to 6
+const numLights = 10; // Set the number of lights to 10
 const lightPositions = [];
-const shiningSpeed = 0.1; // Adjust the shining speed (higher values make it faster)
-const lightStates = [];
+let christmasLights = [];
 
 let treeImage;
 
@@ -89,10 +132,7 @@ function setup() {
     const lightX = random(width * 0.603227, width * 0.453227);
     const lightY = random(height * 0.430939, height * 0.730939);
     lightPositions.push(createVector(lightX, lightY));
-  }
-
-  for (let i = 0; i < numLights; i++) {
-    lightStates.push(255); // Set initial brightness to maximum
+    christmasLights.push(new Light(lightX, lightY, 15)); // Radius set to 15
   }
 }
 
@@ -101,33 +141,11 @@ function draw() {
   imageMode(CORNER);
   image(treeImage, 0, 0, width, height);
 
-  for (let i = 0; i < numLights; i++) {
-    const x = lightPositions[i].x;
-    const y = lightPositions[i].y;
-
-    // Update shining effect with a random factor to create twinkling effect
-    lightStates[i] = (lightStates[i] + random(-10, 10)) % 255;
-
-    // Draw shining effect (diamond shape) with lowered opacity on edges
-    const radius = 15; // Base radius of the diamond shape
-    const angleStep = TWO_PI / 4; // Angle step for diamond shape (4 points)
-    const brightness = map(lightStates[i], 0, 255, 150, 255); // Increased brightness
-    const edgeOpacity = 50; // Opacity of the diamond edges
-    fill(255, 255, 255, brightness); // White glare
-    noStroke();
-    beginShape();
-    for (let j = 0; j < 4; j++) {
-      let angle = j * angleStep;
-      let xOff = cos(angle) * radius;
-      let yOff = sin(angle) * radius;
-      let alpha = j % 2 === 0 ? 255 : edgeOpacity; // Lower opacity on edges
-      fill(255, 255, 255, alpha);
-      vertex(x + xOff, y + yOff);
-    }
-    endShape(CLOSE);
-
-    // Draw base light
-    fill(255);
-    ellipse(x, y, 6, 6);
+  for (let light of christmasLights) {
+    light.update();
+    light.rotate();
+    light.pulse();
+    light.drawLight();
   }
 }
+
